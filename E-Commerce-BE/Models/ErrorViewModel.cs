b95475c797f137959c77d9ef23a8b236056730cf
dihaxn <ihanlaknuka@gmail.com>
2025-07-29 15:35:33 +0530

    Initial commit

---
+++ E-Commerce-BE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commerce_BE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ E-Commerce-BE/Services/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce_BE.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_BE.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Product> Products /*dbname*/ { get; set; }
+
+    }
+}

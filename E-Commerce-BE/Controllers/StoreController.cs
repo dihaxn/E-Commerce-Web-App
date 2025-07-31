7675be356a0693f27b547c41451243d0ae1b1458
dihaxn <ihanlaknuka@gmail.com>
2025-07-31 13:20:26 +0530

    make the store

--- E-Commerce-BE/Controllers/StoreController.cs
+++ E-Commerce-BE/Controllers/StoreController.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿//using Microsoft.AspNetCore.Http;
+using E_Commerce_BE.Models;
+using E_Commerce_BE.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_BE.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StoreController : ControllerBase
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class StoreController : Controller
     {
+        private readonly ApplicationDbContext context;
+        private readonly int pageSize = 8;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index(int pageIndex)
+        {
+            IQueryable<Product> query = context.Products;
+
+            query = query.OrderByDescending(p => p.Id);
+
+            // pagination functionality
+            if (pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            var products = query.ToList();
+
+            ViewBag.Products = products;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
+            return View();
+        }
     }
 }
--- E-Commerce-BE/obj/Debug/net8.0/E-Commerce-BE.AssemblyInfo.cs
+++ E-Commerce-BE/obj/Debug/net8.0/E-Commerce-BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1be06eb194323433e554f83def82b0b9f2a8c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6809da4bf7b6dad6cf21566887500870e4c30b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

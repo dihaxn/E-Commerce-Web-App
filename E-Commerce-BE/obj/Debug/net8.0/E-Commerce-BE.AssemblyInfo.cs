6809da4bf7b6dad6cf21566887500870e4c30b0e
dihaxn <ihanlaknuka@gmail.com>
2025-07-31 11:57:10 +0530

    add css

--- E-Commerce-BE/obj/Debug/net8.0/E-Commerce-BE.AssemblyInfo.cs
+++ E-Commerce-BE/obj/Debug/net8.0/E-Commerce-BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2201870eaee127ee5bd580fc5f774d6a8b03ca5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1be06eb194323433e554f83def82b0b9f2a8c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce-BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-﻿INSERT INTO Products (Name, Brand, Category, Price, Description, ImageFileName, CreatedAt) VALUES
-('Wireless Mouse', 'Logitech', 'Electronics', 2999.99, 'Ergonomic wireless mouse', 'mouse.jpg', GETDATE()),
-('Gaming Keyboard', 'Razer', 'Electronics', 10999.49, 'Mechanical keyboard with RGB lighting', 'keyboard.jpg', GETDATE()),
-('Running Shoes', 'Nike', 'Footwear', 8499.75, 'Comfortable running shoes', 'shoes.jpg', GETDATE()),
-('Bluetooth Speaker', 'JBL', 'Electronics', 6999.00, 'Portable Bluetooth speaker with bass', 'speaker.jpg', GETDATE()),
-('Backpack', 'Samsonite', 'Accessories', 4599.99, 'Stylish and durable backpack', 'backpack.jpg', GETDATE()),
-('Smart Watch', 'Apple', 'Wearable', 125000.00, 'Apple Watch Series 9', 'watch.jpg', GETDATE()),
-('LED Monitor', 'Samsung', 'Electronics', 35999.95, '24-inch Full HD LED monitor', 'monitor.jpg', GETDATE()),
-('Desk Lamp', 'Philips', 'Home', 1899.20, 'Adjustable LED desk lamp', 'lamp.jpg', GETDATE());
